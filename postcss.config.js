f5eccfeeafae9fd47050c1d2c6b6a138ad5accb1
Anton Melnyk <pixelgoo@gmail.com>
2021-02-14 02:31:07 +0200

    Apply postcss for development mode for visibility and to avoid webpack warning

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
-if(process.env.NODE_ENV === 'production') {
+// Uncomment if statement to disable minifying and prefixing during development!
+
+// if (process.env.NODE_ENV === 'production') {
     module.exports = {
         plugins: [
             require('autoprefixer'),
             require('cssnano')
         ]
     }
-}
+// }
